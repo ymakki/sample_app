b3c2a720d70085681f897d07193ab67cf6b5d48c
ymakki <147698184+ymakki@users.noreply.github.com>
2023-12-09 06:09:21 +0000

    [Finish] アプリケーションを完成させよう1章

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get 'homes/top' => 'homes#top'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
