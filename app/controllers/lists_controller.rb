eaba758287064d484113d57bfee06836802af3b1
ymakki <147698184+ymakki@users.noreply.github.com>
2024-01-11 06:27:55 +0000

    [Finish]【Rails】find, find_by, whereを理解しよう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,39 @@ class ListsController < ApplicationController
   end
 
   def show
+    # 「findメソッド」は主キー(id)を指定して、見つかった1レコードを返す
+      # 1.find()の()内に記述できる数値（id）のみ
+      # 2.idは主キーなので、1レコードのみ取得
+      # 3.検索に該当するデータがなかった場合、ActiveRecord::RecordNotFoundを返す
+
+      # id=1のみを取得したい場合
+      # List.find(1)
+
+      # URLの:idを取得したい場合
+      # List.find(params[:id])
+
+    # 「find_byメソッド」は主キー(id)以外のカラムを指定しても、見つかった1レコードを返せる
+      # 1.find_by()の()内に記述できるのは、数値（id）だけでなく、文字（id以外のカラム）も可能
+      # 2.id以外のカラムも指定できるので、複数のレコードが見つかる場合もありますが、その時は、一番最初に見つかったレコード1件を取得
+      # 3.検索に該当するデータがなかった場合、nilを返す
+
+      # titleカラムに「ruby」で投稿されたレコードを取得したい場合
+      # List.find_by(title: "ruby")
+
+      # titleカラムに「ruby」、bodyカラムに「rails」で投稿されたレコードを取得したい場合
+      # List.find_by(title: "ruby",body: "rails")
+
+    # 「whereメソッド」は主キー(id)以外のカラムも指定できる、且つ、複数のレコードも返せる
+      # 1.where()の()内に記述できるのは、数値（id）だけでなく、文字（id以外のカラム）も可能
+      # 2.該当する複数のレコードを取得
+      # 3.検索に該当するデータがなかった場合、空の配列を返す
+
+      # titleカラムに「ruby」で投稿された全レコードを取得したい場合
+      # List.where(title: "ruby")
+
+      # titleカラムに「ruby」、bodyカラムに「rails」で投稿された全レコードを取得したい場合
+      # List.where(title: "ruby",body: "rails")
+
      @list = List.find(params[:id])
   end
 
@@ -32,7 +65,7 @@ class ListsController < ApplicationController
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)
+    redirect_to list_path(list)
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2023_12_09_063801) do
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
